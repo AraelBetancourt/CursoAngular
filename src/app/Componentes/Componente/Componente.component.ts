856940dd04bcd3554c1a526e040b883f35c7d3e3
Jose Arael Betancourt Flores <arael.betancourt@hotmail.com>
2021-10-07 18:58:56 -0500

    Alta de Proyecto

---
+++ src/app/Componentes/Componente/Componente.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+@Component({
+    selector:'app-componente',
+    templateUrl:'./Componente.component.html',
+    styleUrls:[ './Componente.component.scss' ]
+})
+
+export class ComponenteComponent{
+
+}
